84934058f7914e3f60555a67299f06e4b303577f
MatthewRSouth <matthewrogersouth@gmail.com>
2026-02-03 06:55:35 +0900

    feat: finished parent information form

--- src/components/ChildPersonalInformationOne.jsx
+++ src/components/ChildPersonalInformationOne.jsx
@@ -1,9 +1,51 @@
 export default function ChildPersonalInfoOne({ onNext, onPrevious }) {
     return (
         <div>
-            <h1>Child Personal Info 1</h1>
-            <button onClick={onNext}>Next</button>
-            <button onClick={onPrevious}>Previous</button>
+            <h1>Child Information お子様の内容</h1>
+            <h4>Child’s Name お子様のお名前　</h4>
+            <div className="form-group">
+                <label htmlFor="childNameEnglish">英語表記 </label>
+                <input
+                    type="text"
+                    id="childNameEnglish"
+                    name="childNameEnglish"
+                    placeholder="Yamda Taro"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="childNameFurigana">フリガナ </label>
+                <input
+                    type="text"
+                    id="childNameFurigana"
+                    name="childNameFurigana"
+                    placeholder="ヤマダ　タロウ"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="childNameKanji">漢字 </label>
+                <input
+                    type="text"
+                    id="childNameKanji"
+                    name="childNameKanji"
+                    placeholder="山田　太郎"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="childAddress">Home Address 住所 </label>
+                <br />
+                <textarea
+                    type="text"
+                    id="childAddress"
+                    name="childAddress"
+                    rows={4}
+                    placeholder="00-0001 東京都千代田区千代田1-1-1 "
+                ></textarea>
+            </div>
+            <div className="button-group">
+                {' '}
+                <button onClick={onNext}>Next</button>
+                <button onClick={onPrevious}>Previous</button>
+            </div>
         </div>
     );
 }
--- src/components/ParentInformation.jsx
+++ src/components/ParentInformation.jsx
@@ -1,9 +1,64 @@
 export default function ParentInformation({ onNext, onPrevious }) {
     return (
         <div>
-            <h1>Parent info</h1>
-            <button onClick={onNext}>Next</button>
-            <button onClick={onPrevious}>Previous</button>
+            <h1>Parent/Guardian Information ご両親の内容</h1>
+            <h4>Father’s Information 父親の情報</h4>
+            <div className="form-group">
+                <label htmlFor="fatherNameFurigana">フリガナ</label>
+                <input
+                    type="text"
+                    id="fatherNameFurigana"
+                    name="fatherNameFurigana"
+                    placeholder="例)　ヤマダ　タロウ"
+                />
+                <label htmlFor="fatherNameKanji">漢字</label>
+                <input
+                    type="text"
+                    id="fatherNameKanji"
+                    name="fatherNameKanji"
+                    placeholder="例)　山田　太郎"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="fatherPhoneNumber">Cell Phone携帯電話</label>
+                <input
+                    type="tel"
+                    id="fatherPhoneNumber"
+                    name="fatherPhoneNumber"
+                    placeholder="例）090-1234-5678"
+                />
+            </div>
+            <h4>Mother’s Information 母親の情報</h4>
+            <div className="form-group">
+                <label htmlFor="motherNameFurigana">フリガナ</label>
+                <input
+                    type="text"
+                    id="motherNameFurigana"
+                    name="motherNameFurigana"
+                    placeholder="例)　ヤマダ　ハナコ"
+                />
+                <label htmlFor="motherNameKanji">漢字</label>
+                <input
+                    type="text"
+                    id="motherNameKanji"
+                    name="motherNameKanji"
+                    placeholder="例)　山田　花子"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="motherPhoneNumber">Cell Phone携帯電話</label>
+                <input
+                    type="tel"
+                    id="motherPhoneNumber"
+                    name="motherPhoneNumber"
+                    placeholder="例）090-1234-5678"
+                />
+            </div>
+            <div className="button-group">
+                {' '}
+                <button onClick={onNext}>Next</button>
+                <button onClick={onPrevious}>Previous</button>
+            </div>
         </div>
     );
 }
