cc943d968865d444bdde7212033f0918b28027d0
MatthewRSouth <matthewrogersouth@gmail.com>
2026-02-04 07:25:25 +0900

    feat: finished all form inputs including signature

--- src/components/Allergies.jsx
+++ src/components/Allergies.jsx
@@ -2,8 +2,36 @@ export default function Allergies({ onNext, onPrevious }) {
     return (
         <div>
             <h1>Allergies</h1>
-            <button onClick={onNext}>Next</button>
-            <button onClick={onPrevious}>Previous</button>
+            <div className="form-group">
+                <label htmlFor="">Does your child have any allergies?</label>
+                <div className="allergies-radio-group">
+                    <label htmlFor="">
+                        <input type="radio" name="hasAllergies" value="yes" />
+                        Yes　有り
+                    </label>
+                    <label htmlFor="">
+                        <input type="radio" name="hasAllergies" value="no" />
+                        No　無い
+                    </label>
+                </div>
+            </div>
+            <div className="form-group">
+                <label htmlFor="allergyDetails">
+                    If yes, please provide details:
+                </label>
+                <br />
+                <textarea
+                    name="allergyDetails"
+                    id="allergyDetails"
+                    cols="30"
+                    rows="4"
+                ></textarea>
+            </div>
+            <div className="button-group">
+                {' '}
+                <button onClick={onNext}>Next</button>
+                <button onClick={onPrevious}>Previous</button>
+            </div>
         </div>
     );
 }
--- src/components/ParentAcknowledgement.jsx
+++ src/components/ParentAcknowledgement.jsx
@@ -1,9 +1,53 @@
 export default function ParentAcknoledgement({ onNext, onPrevious }) {
     return (
         <div>
-            <h1>Parent Acknowledgement</h1>
-            <button onClick={onNext}>Next</button>
-            <button onClick={onPrevious}>Previous</button>
+            <h1>Parent Acknowledgement 保護者の承諾</h1>
+            <div className="acknowledgement-checkbox-group">
+                <label htmlFor="">
+                    <p>
+                        I understand and accept that under no circumstances
+                        shall refunds be given. <br />
+                        私は入学の申し込みに関して発生する費用は、返還されないことを理解します。
+                        <br />
+                        <br />
+                        I understand and accept that under no circumstances I
+                        shall receive refunds when the government forces the
+                        school to close down for a certain period. <br />
+                        私は役所の指示で休園する場合に関して授業料等の費用は、返還されないことを理解します。
+                        <br /> <br /> I declare that the information given in
+                        this application form is true and accurate <br />
+                        私はこの入学願書に記述した内容について事実と相違ないことを誓います。
+                    </p>
+                    <input type="checkbox" name="agreesToAcknowledgement" /> I
+                    acknowledge
+                </label>
+            </div>
+            <div className="form-group">
+                <p className="achknowledgement-legal-text">
+                    By typing my name below, I electronically sign this
+                    application and attest that all information provided is true
+                    and accurate.
+                </p>
+                <label htmlFor="signature">Parents Signature 保護者署名 </label>
+                <input
+                    type="text"
+                    id="signature"
+                    name="signature"
+                    placeholder="Type your full name here"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="signDate">
+                    Date of Application 願書記入日{' '}
+                </label>
+                <input type="date" id="signDate" name="signDate" />
+            </div>
+
+            <div className="button-group">
+                {' '}
+                <button onClick={onNext}>Next</button>
+                <button onClick={onPrevious}>Previous</button>
+            </div>
         </div>
     );
 }
