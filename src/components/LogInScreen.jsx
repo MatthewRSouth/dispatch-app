53bca6597c1f74232564ef1e7e23d168be477f1b
MatthewRSouth <matthewrogersouth@gmail.com>
2026-02-04 07:44:44 +0900

    feat: complete login and all input forms

--- src/components/LogInScreen.jsx
+++ src/components/LogInScreen.jsx
@@ -1,8 +1,36 @@
 export default function LogInScreen({ onNext }) {
     return (
-        <div>
-            <h1>Log-in</h1>
-            <button onClick={onNext}>Next</button>
+        <div className="login-container">
+            <div className="login-image">
+                <div className="image-overlay"></div>
+            </div>
+            <div className="login-form-section">
+                <h2>sign-in</h2>
+                <p className="login-instruction">
+                    The username and password are provided on the sign-up forms.
+                </p>
+            </div>
+            <div className="form-group">
+                <label htmlFor="username">Username: </label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Enter username"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password: </label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter Password"
+                />
+            </div>
+            <button className="login-button" onClick={onNext}>
+                Log In
+            </button>
         </div>
     );
 }
