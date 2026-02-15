1f153f09585d4dae401a83b94617b14291c66b4a
MatthewRSouth <matthewrogersouth@gmail.com>
2026-02-16 07:58:05 +0900

    chore:success page skeleton and component made

--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -1,8 +1,36 @@
-export default function Success({ onPrevious }) {
+import '../styles/success.css';
+import './Footer';
+
+function Card({ image, title, description, alt }) {
     return (
-        <div>
-            <h1>Success!</h1>
-            <button onClick={onPrevious}>Previous</button>
+        <div className="next-steps-content">
+            <div className="card">
+                <img src={image} alt={alt} />
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
         </div>
+    );
+}
+
+export default function SuccessMessage({ onPrevious }) {
+    return (
+        <>
+            <header className="header-container">
+                <img src="" alt="" className="header-image" />
+                <h1 className="header-text">Submission Succesful</h1>
+                <hr className="header-line" />
+            </header>
+            <div className="next-steps-container">
+                <h2>Please follow these next steps</h2>
+                <Card
+                    alt="Line QR Code"
+                    title="Line"
+                    description="Please follow us on Line so we can give you the most up-to-date information"
+                ></Card>
+                <Card title="Payment"></Card>
+                <Card title="Website"></Card>
+            </div>
+        </>
     );
 }
