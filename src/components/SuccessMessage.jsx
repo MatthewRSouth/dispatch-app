43c142da96f5b4419bbec749d0816cb929a761dd
MatthewRSouth <matthewrogersouth@gmail.com>
2026-02-17 06:16:17 +0900

    feat: success page implemented

--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -1,13 +1,14 @@
 import '../styles/success.css';
+import '..//styles/variables.css';
 import './Footer';
 
 function Card({ image, title, description, alt }) {
     return (
         <div className="next-steps-content">
             <div className="card">
-                <img src={image} alt={alt} />
-                <h3>{title}</h3>
-                <p>{description}</p>
+                <img src={image} alt={alt} className="card-image" />
+                <h3 className="card-title">{title}</h3>
+                <p className="card-description">{description}</p>
             </div>
         </div>
     );
@@ -17,7 +18,11 @@ export default function SuccessMessage({ onPrevious }) {
     return (
         <>
             <header className="header-container">
-                <img src="" alt="" className="header-image" />
+                <img
+                    src="src/assets/check-circle-svgrepo-com (2).svg"
+                    alt=""
+                    className="header-image"
+                />
                 <h1 className="header-text">Submission Succesful</h1>
                 <hr className="header-line" />
             </header>
@@ -28,8 +33,17 @@ export default function SuccessMessage({ onPrevious }) {
                     title="Line"
                     description="Please follow us on Line so we can give you the most up-to-date information"
                 ></Card>
-                <Card title="Payment"></Card>
-                <Card title="Website"></Card>
+                <Card
+                    image="src/assets/yen-circle-svgrepo-com.svg"
+                    title="Payment"
+                    description="Please make your payment at this bank"
+                ></Card>
+                <Card
+                    image="src/assets/hoshida-dispatch-qr-code.png"
+                    alt="website qr code"
+                    title="Website"
+                    description="Please see our website for any other information you need."
+                ></Card>
             </div>
         </>
     );
